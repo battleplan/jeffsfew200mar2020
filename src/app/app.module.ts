ff6f04215372d8ce69e671b30909f55a358cbeec
Jeffry Gonzalez <jeff@hypertheory.com>
2020-03-23 14:55:15 -0400

    end of day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ShoppingEntryComponent } from './components/shopping/components/shopping-entry/shopping-entry.component';
+import { ShoppingListComponent } from './components/shopping/components/shopping-list/shopping-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ShoppingComponent,
+    NavComponent,
+    ShoppingEntryComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
